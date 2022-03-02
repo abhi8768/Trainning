c46f8fc7751cd1fda9934ed044bf8b283c358da3
abhi8768 <dolui.abhishek@gmail.com>
2022-03-02 09:00:24 +0530

    Session 3 - Form API - Exercises

---
+++ web/modules/custom/conSimpleForm/src/Form/SimpleForm.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace Drupal\conSimpleForm\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * This class is generate a simple form.
+ */
+class SimpleForm extends FormBase
+{
+
+  /**
+   * This function is for form id.
+   *
+   * @return formId
+   *   Return only form id.
+   */
+  public function getFormId()
+  {
+    return 'conSimpleForm';
+  }
+
+  /**
+   * Build form fields.
+   *
+   * @param array $form
+   *   Form buldup array.
+   * @param FormStateInterface $form_state
+   *   An.
+   *
+   * @return form
+   *   Form fields.
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
+    $countryManager = \Drupal::service('country_manager');
+    $list = $countryManager->getList();
+    $countries = [];
+    foreach ($list as $key => $value) {
+      $val = $value->__toString();
+      $countries[$val] = $val;
+    }
+
+    $form['country'] = [
+      '#title' => t('Countries'),
+      '#type' => 'select',
+      '#options' => $countries,
+    ];
+
+    $form['#tree'] = TRUE;
+    $form['names_fieldset'] = [
+      '#type' => 'fieldset',
+      '#prefix' => '<div id="names-fieldset-wrapper">',
+      '#suffix' => '</div>',
+    ];
+
+    if (empty($form_state->get('num_names'))) {
+      $form_state->set('num_names', 1);
+    }
+    for ($i = 0; $i < $form_state->get('num_names'); $i++) {
+      $form['names_fieldset'][$i]['state'] = [
+        '#type' => 'textfield',
+        '#title' => $this->t('State'),
+        '#pattern' => '[A-Za-z]*',
+      ];
+      $form['names_fieldset'][$i]['city'] = [
+        '#type' => 'textfield',
+        '#title' => $this->t('City'),
+        '#pattern' => '[A-Za-z]*',
+      ];
+    }
+
+    $form['add_name'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Add one more'),
+      '#submit' => ['::addMore'],
+      '#ajax' => [
+        'callback' => '::addMoreCallback',
+        'wrapper' => 'names-fieldset-wrapper',
+      ],
+    ];
+
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Submit'),
+    ];
+    return $form;
+  }
+  /**
+   * Add more row function.
+   *
+   * @param array $form
+   *   Form buldup array.
+   * @param FormStateInterface $form_state
+   *   An.
+   */
+  public function addMore(array $form, FormStateInterface $form_state)
+  {
+    $currentNum = $form_state->get('num_names');
+    $currentNum = $currentNum + 1;
+    $form_state->set('num_names', $currentNum);
+    $form_state->setRebuild();
+  }
+  /**
+   * Add more call back function.
+   *
+   * @param array $form
+   *   Form buldup array.
+   * @param FormStateInterface $form_state
+   *   An.
+   *
+   * @return returnValue
+   *   Return name field set.
+   */
+  public function addMoreCallback(array $form, FormStateInterface $form_state)
+  {
+    $returnValue = $form['names_fieldset'];
+    return $returnValue;
+  }
+
+  /**
+   * Submit handler function.
+   *
+   * @param array $form
+   *   Form buldup array.
+   * @param FormStateInterface $form_state
+   *   An.
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state)
+  {
+    $country = $form_state->getValue('country');
+    \Drupal::messenger()->addMessage('Your selected country is : ' . $country);
+    \Drupal::messenger()->addMessage('Below are the locations you have entered');
+    foreach ($form_state->getValue('names_fieldset') as $value) {
+      for ($i = 0; $i < count($value); $i++) {
+        \Drupal::messenger()->addMessage($value['state'] . "," . $value["city"]);
+      }
+    }
+  }
+
+}
